a6d2693527f5845992e9bb100a042e5113e229a6
Amir Khamis <54420679+amir-khamis@users.noreply.github.com>
2020-01-31 10:32:25 -0500

    Fixed du permissions issue

--- bin/monitor_lookup_files_apps.sh
+++ bin/monitor_lookup_files_apps.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-find /opt/splunk/etc/apps -maxdepth 3 -path '*/lookups/*' -name "*.csv" | xargs du -sb | grep -v "^0" | sort | awk -F "/" '{"date \"+%m-%d-%Y %H:%M:%S.%3N %z\""| getline date; print date, "INFO", "app="$6, "file_name="$8, "file_size="$1}'
+
+find /opt/splunk/etc/apps -maxdepth 3 -path '*/lookups/*' -name "*.csv" | awk '{print "\""$0"\""}' | xargs du -sb 2> >(grep -v 'Permission denied') | grep -v "^0" | sort | awk -F "/" '{"date \"+%m-%d-%Y %H:%M:%S.%3N %z\""| getline date; print date, "INFO", "app="$6, "file_name="$8, "file_size="$1}'
